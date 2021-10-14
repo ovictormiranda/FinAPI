a0fba9a4bddc20a90a2396fddfd0ddd0bb3aeef9
ovictormiranda <carreira.victormiranda@gmail.com>
2021-10-13 21:48:02 -0300

    Step 1

---
+++ consulta.js
@@ -0,0 +1,45 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+// cpf - String
+// name - String
+// id - uuid
+// statement - []
+
+app.post("/account", (request, response) => {
+  const cpf = request.body;
+});
+
+
+app.listen(3333);
+
+
+ app.use(express.json());
+
+ app.get("/courses", (request, response) => {
+   const query = request.query;
+   console.log(query);
+   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+ });
+
+ app.post("/courses", (request, response) => {
+   const body = request.body;
+   console.log(body);
+   return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+ });
+
+ app.put("/courses/:id", (request, response) => {
+   const { id } = request.params;
+   console.log(id);
+   return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+ });
+
+ app.patch("/courses/:id", (request, response) => {
+   return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+ });
+
+ app.delete("/courses/:id", (request, response) => {
+   return response.json(["Curso 6", "Curso 7", "Curso 4"]);
+ })
